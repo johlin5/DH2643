6740a95522715d92cae9545bd95fb8a27803da6c
Robin Dang <rpdang@kth.se>
2021-09-09 17:32:32 +0200

    init webpack

---
+++ project/client/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist/index.html",
+    port: 9000,
+    allowedHosts: "all",
+  },
+};
