7e2c2bb4ea817eea379e792880034f0e7435ba61
Robin Dang <rpdang@kth.se>
2021-09-13 19:12:17 +0200

    Added express session,cookies and middleware config

--- project/server/server.js
+++ project/server/server.js
@@ -1,16 +1,33 @@
 /**
  * SETUP
  */
-const PORT = process.env.PORT || 8080,
-  betterLogging = require("better-logging"),
+const betterLogging = require("better-logging"),
+  cookieParser = require("cookie-parser"),
+  path = require("path"),
   express = require("express"),
+  expressSession = require("express-session"),
   app = express();
 
+/**
+ * SETTINGS
+ */
+const PORT = process.env.PORT || 8080;
+const publicPath = path.resolve(__dirname, "../client/dist/");
 const { Theme } = betterLogging;
 betterLogging(console, {
   color: Theme.green,
 });
-console.logLevel = 4;
+console.logLevel = 4; // debug level
+const session = expressSession({
+  secret: "toptoptopsecret",
+  name: "Quiz",
+  resave: true,
+  saveUninitialized: true,
+});
+
+/**
+ * MIDDLEWARES
+ */
 app.use(
   betterLogging.expressMiddleware(console, {
     ip: { show: true, color: Theme.green.base },
@@ -20,5 +37,36 @@ app.use(
     body: { show: true },
   })
 );
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser("toptoptopsecret"));
+app.use(express.json());
+app.use(session);
+app.use(express.static(publicPath));
 
-app.listen(PORT, () => console.debug(`Listening on http://localhost:${PORT}`));
+// MIDDLEWARE DEBUG
+app.use((req, res, next) => {
+  const cookie = req.cookies.quiz;
+  if (!cookie) {
+    let randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2, randomNumber.length);
+    res.cookie("quiz", randomNumber, {
+      maxAge: 900000,
+      httpOnly: true,
+    });
+  }
+  console.debug(cookie);
+  next();
+});
+app.use("*", (req, res, next) => {
+  console.debug("REQ SESSION", req.session);
+  next();
+});
+
+/**
+ * API BINDINGS
+ */
+
+/**
+ * Awaaaay we goooooooooooooo!!!!!!
+ */
+app.listen(PORT, () => console.info(`Listening on http://localhost:${PORT}`));
