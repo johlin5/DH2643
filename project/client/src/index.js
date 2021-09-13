6ebfd0439174485ded24c615e26806ba098efb05
Robin Dang <rpdang@kth.se>
2021-09-13 17:13:35 +0200

    cleanup and rmeoved generated files, todo config tsc compilation

--- project/client/src/index.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import "./styles";
-import App from "./components/App";
-import { render } from "react-dom";
-render(_jsx(App, {}, void 0), document.getElementById("root"));
