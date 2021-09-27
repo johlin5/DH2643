94131aa6b99b23aeba7dc688e593b69a5d197442
Johan Besseling <johanbesseling@Johans-MacBook-Pro.local>
2021-09-27 22:42:13 +0200

    added basic protected routes, might have to replace sessions, depending on how we choose to store authentication data with JWT

--- project/client/src/app/Layout.tsx
+++ project/client/src/app/Layout.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { navRoutes } from "./Routes";
+// Not sure if we are gonna use sessions to store status of authentication
+// Could be used to store status of JWT, which is stored in the browser.
+import { useSessionContext } from "../services/contexts/SessionContext";
+import ProtectedRoute, { ProtectedRouteProps } from "../components/ProtectedRoute";
 import Header from "../components/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 
 const Layout: React.FC = () => {
+  const [sessionContext, updateSessionContext] = useSessionContext();
+
+  const setRedirectPath = (path: string) => {
+    updateSessionContext({...sessionContext, redirectPath: path});
+  }
+
+  const defaultProtectedRouteProps: ProtectedRouteProps = {
+    isAuthenticated: !!sessionContext.isAuthenticated,
+    authenticationPath: '/login',
+    redirectPath: sessionContext.redirectPath,
+    setRedirectPath: setRedirectPath
+  };
+
   return (
     <Router>
       <CssBaseline />
@@ -16,6 +33,23 @@ const Layout: React.FC = () => {
       </Switch>
     </Router>
   );
+
+    // return (
+    //   <Router>
+    //     <CssBaseline />
+    //     <Header />
+    //     <Switch>
+    //       {navRoutes.map((nav) => {
+    //         return (
+    //           nav.path === '/login' || nav.path === '/' ? <Route path={nav.path} exact={nav.exact} component={nav.component}></Route>  :
+    //           <ProtectedRoute {...defaultProtectedRouteProps} path={nav.path} exact={nav.exact} component={nav.component}/>
+    //         )
+    //       })}
+    //     </Switch>
+    //   </Router>
+    // );
 };
+
+
 
 export default Layout;
---
+++ project/client/src/services/models/session.ts
@@ -0,0 +1,9 @@
+export type Session = {
+    isAuthenticated?: boolean;
+    redirectPath: string;
+  }
+  
+  export const initialSession: Session = {
+    redirectPath: ''
+  };
+  
