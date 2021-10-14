2ff228936f180230aa9da6639c09a09a150b5c16
David Yu <Davyie@n130-p110.eduroam.kth.se>
2021-10-14 11:20:30 +0200

    Wrote small code snippets such that creator is set by default when creating quiz

--- project/client/src/views/CreateQuiz.tsx
+++ project/client/src/views/CreateQuiz.tsx
@@ -3,12 +3,18 @@ import { GREEN } from "../app/theme";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 import Quiz from "../views/quiz/Index";
+import { useRecoilValue } from "recoil";
+import { withUserName } from "../selectors/account";
 
 const CreateQuiz: React.FC = () => {
   const [addingQuiz, setAddingQuiz] = useState(false);
+  const username = useRecoilValue(withUserName);
+
+  const newQuiz = {title: "", questions: [], creator: username}; 
+  
   return (
     <Container>
-        {addingQuiz ? <Quiz quiz={{title: "", questions: [], creator: ""}} editState={addingQuiz}/> 
+        {addingQuiz ? <Quiz quiz={newQuiz} editState={addingQuiz}/> 
         : <PrimaryButton text="Add" color={GREEN} variant="h6" height="48px" onClick={() => setAddingQuiz(true)} />}
     </Container>
   );
