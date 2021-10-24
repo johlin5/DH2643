b7dc8eda8b86d8bd1e54d565fd1d0ba3d2683ec1
David Yu <Davyie@n130-p110.eduroam.kth.se>
2021-10-24 11:35:26 +0200

    Deleted all the Views from create quiz

--- project/client/src/app/Routes.tsx
+++ project/client/src/app/Routes.tsx
@@ -3,7 +3,6 @@ import Login from "../views/login/LoginPresenter";
 import Profile from "../views/profile";
 import Landing from "../views/Landing";
 import Register from "../views/register/Index";
-import Quizzes from "../views/Quizzes";
 import CreateQuiz from "../views/CreateQuiz";
 import Play from "../views/game";
 
@@ -22,7 +21,6 @@ export const unprotectedRoutes: AppRoute[] = [
   { path: "/", component: Landing, exact: true, requireLoggedIn: false },
   { path: "/login", component: Login, exact: true, requireLoggedIn: false },
   { path: "/register", component: Register, exact: true, requireLoggedIn: false },
-  { path: "/quizes", component: Quizzes, exact: true, requireLoggedIn: false },
   { path: "*", component: NotFound, exact: false, requireLoggedIn: false }
 ];
 
@@ -30,8 +28,6 @@ export const protectedRoutes: AppRoute[] = [
   { path: "/", component: DashboardPresenter, exact: true, requireLoggedIn: true },
   { path: "/profile", component: Profile, exact: true, requireLoggedIn: true },
   { path: "/createquiz", component: CreateQuiz, exact: true, requireLoggedIn: true },
-  { path: "/myquizes", component: Quizzes, exact: true, requireLoggedIn: true },
   { path: "/play/:id", component: Play, exact: false, requireLoggedIn: true },
-
   { path: "*", component: NotFound, exact: false, requireLoggedIn: false }
 ];
